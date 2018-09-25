cae0d6b67385c4fcf9b0227c22b3bcf1c9d76d81
Spencer Hawkins <hawkins.spencer@gmail.com>
2018-09-25 01:12:46 -0700

    fix broken https upgrade

--- js/main.js
+++ js/main.js
@@ -5,9 +5,8 @@ window.toastr = toastr
 
 // https://stackoverflow.com/questions/4723213/detect-http-or-https-then-force-https-in-javascript
 if (location.protocol !== "https:" && location.hostname != "localhost") {
-  location.protocol = "https:";
   window.stop()
-  throw new Error("Forcing to https, throwing an error to prevent further JS from executing");
+  location.protocol = "https:";
 }
 
 var appData = {
