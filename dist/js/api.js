ba1b833557ffcbfa2839a8b232eb263cede28bb9
Spencer Hawkins <hawkins.spencer@gmail.com>
2018-10-06 01:33:10 -0700

    move inspector to separate goextend container

--- dist/js/api.js
+++ dist/js/api.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var API_URL = "https://gx3.mtgatracker.com/str-85b6a06b2d213fac515a8ba7b582387a-p3/mtgatracker-prod-EhDvLyq7PNb";
+var API_URL = "https://gx2.mtgatracker.com/str-85b6a06b2d213fac515a8ba7b582387a-p2/mtgatracker-prod-EhDvLyq7PNb";
 
 var cookies = require('browser-cookies');
 
