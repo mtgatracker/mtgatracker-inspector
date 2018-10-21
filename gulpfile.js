4c5341bf85094ec06722696e721a2860dfcea25f
Spencer Hawkins <hawkins.spencer@gmail.com>
2018-10-20 18:38:24 -0700

    gulp -> 4.0.0; npm audits; add admin html (lol)

--- gulpfile.js
+++ gulpfile.js
@@ -14,38 +14,32 @@ var buffer = require("vinyl-buffer");
 var sourcemaps = require("gulp-sourcemaps");
 
 // Compile LESS files from /less into /css
-gulp.task('less', function() {
+function compileLess() {
     return gulp.src('less/style.less')
         .pipe(less())
         .pipe(gulp.dest('dist/css'))
-        .pipe(browserSync.reload({
-            stream: true
-        }))
-});
+}
+gulp.task('less', compileLess);
 
 // Minify compiled CSS
-gulp.task('minify-css', ['less'], function() {
+function minifyCSS() {
     return gulp.src('dist/css/style.css')
         .pipe(cleanCSS({ compatibility: 'ie8' }))
         .pipe(rename({ suffix: '.min' }))
         .pipe(gulp.dest('dist/css'))
-        .pipe(browserSync.reload({
-            stream: true
-        }))
-});
+}
+gulp.task('minify-css', gulp.series('less', minifyCSS));
 
 // Copy JS to dist
-gulp.task('js', function() {
+function copyJS() {
     return gulp.src(['js/**/*.js'])
         .pipe(babel())
         .pipe(gulp.dest('dist/js'))
-        .pipe(browserSync.reload({
-            stream: true
-        }))
-})
+}
+gulp.task('js', copyJS)
 
 // Minify JS
-gulp.task('minify-js', ['js'], function() {
+function minifyJS() {
   // set up the browserify instance on a task basis
   var b = browserify({
     entries: './js/main.js',
@@ -62,13 +56,14 @@ gulp.task('minify-js', ['js'], function() {
     .pipe(rename({ suffix: '.min' }))
     .pipe(sourcemaps.write('./maps'))
     .pipe(gulp.dest('./dist/js/'));
-});
+}
+
+gulp.task('minify-js', gulp.series('js', minifyJS));
 
 // CACHE BUSTER
 // cacheBuster looks at the css and js files and appends a hash to the
 // request to cause the file to get reloaded when the file changes.
-gulp.task('cache-bust', ['minify-css', 'minify-js'], function () {
-
+function cacheBust() {
     gulp.src(['deck/index.html'])
         .pipe(cacheBuster())
           .on('error', gulpUtil.log)
@@ -84,68 +79,35 @@ gulp.task('cache-bust', ['minify-css', 'minify-js'], function () {
           .on('error', gulpUtil.log)
         .pipe(gulp.dest('login/'));
 
-    gulp.src(['index.html'])
+    return gulp.src(['index.html'])
         .pipe(cacheBuster())
           .on('error', gulpUtil.log)
         .pipe(gulp.dest('./'));
-});
+}
 
-// Copy vendor libraries from /bower_components into /vendor
-gulp.task('copy', function() {
-    gulp.src(['bower_components/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
-        .pipe(gulp.dest('vendor/bootstrap'))
-
-    gulp.src(['bower_components/bootstrap-social/*.css', 'bower_components/bootstrap-social/*.less', 'bower_components/bootstrap-social/*.scss'])
-        .pipe(gulp.dest('vendor/bootstrap-social'))
-
-    gulp.src(['bower_components/datatables/media/**/*'])
-        .pipe(gulp.dest('vendor/datatables'))
-
-    gulp.src(['bower_components/datatables-plugins/integration/bootstrap/3/*'])
-        .pipe(gulp.dest('vendor/datatables-plugins'))
-
-    gulp.src(['bower_components/datatables-responsive/css/*', 'bower_components/datatables-responsive/js/*'])
-        .pipe(gulp.dest('vendor/datatables-responsive'))
-
-    gulp.src(['bower_components/flot/*.js'])
-        .pipe(gulp.dest('vendor/flot'))
-
-    gulp.src(['bower_components/flot.tooltip/js/*.js'])
-        .pipe(gulp.dest('vendor/flot-tooltip'))
-
-    gulp.src(['bower_components/font-awesome/**/*', '!bower_components/font-awesome/*.json', '!bower_components/font-awesome/.*'])
-        .pipe(gulp.dest('vendor/font-awesome'))
-
-    gulp.src(['bower_components/jquery/dist/jquery.js', 'bower_components/jquery/dist/jquery.min.js'])
-        .pipe(gulp.dest('vendor/jquery'))
-
-    gulp.src(['bower_components/metisMenu/dist/*'])
-        .pipe(gulp.dest('vendor/metisMenu'))
-
-    gulp.src(['bower_components/morrisjs/*.js', 'bower_components/morrisjs/*.css', '!bower_components/morrisjs/Gruntfile.js'])
-        .pipe(gulp.dest('vendor/morrisjs'))
-
-    gulp.src(['bower_components/raphael/raphael.js', 'bower_components/raphael/raphael.min.js'])
-        .pipe(gulp.dest('vendor/raphael'))
-
-})
+gulp.task('cache-bust', gulp.series('minify-css', 'minify-js', cacheBust));
 
 // Run everything
-gulp.task('default', ['minify-css', 'minify-js', 'copy', 'cache-bust']);
+gulp.task('default', gulp.series(gulp.parallel(minifyCSS, minifyJS), cacheBust));
+
+function reloadBrowser(done) {
+  browserSync.reload()
+  done()
+}
 
 // Configure the browserSync task
-gulp.task('browserSync', function() {
+gulp.task('browserSync', function(done) {
     browserSync.init({
         server: ".",
     })
+    done()
 })
 
+function watchFiles() {
+    gulp.watch('less/*.less', gulp.series('minify-css', reloadBrowser));
+    gulp.watch('js/**/*.js').on('change', gulp.series('minify-js'));
+    return gulp.watch('dist/js/bundle.min.js', reloadBrowser);
+}
+
 // Dev task with browserSync
-gulp.task('dev', ['less', 'minify-css', 'js', 'minify-js', 'cache-bust', 'browserSync'], function() {
-    gulp.watch('less/*.less', ['less']);
-    gulp.watch('dist/css/*.css', ['minify-css']);
-    gulp.watch('js/**/*.js', ['minify-js']);
-    // Reloads the browser whenever HTML or JS files change
-//    gulp.watch('pages/*.html', browserSync.reload);
-    gulp.watch('dist/js/bundle.min.js', browserSync.reload);
-});
+gulp.task('dev', gulp.series('default', 'browserSync', watchFiles));
